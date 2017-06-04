46f877bc83dddb2d11a1cee3b0cdb69fdbd07e0e
Kavin Arasu <mailkavin@gmail.com>
2017-06-03 22:06:34 -0700

    Configure dropwizard for PAM service

---
+++ pam-service-core/src/main/java/com/redflamelabs/pam/service/PamServiceConfiguration.java
@@ -0,0 +1,11 @@
+package com.redflamelabs.pam.service;
+
+import io.dropwizard.Configuration;
+
+/**
+ * Configuration for the PAM service
+ *
+ * Created by kavinarasu on 6/3/17.
+ */
+public class PamServiceConfiguration extends Configuration {
+}
