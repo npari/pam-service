6a554bbb251ed3f5f02c35217efab00697cbefc8
Kavin Arasu <mailkavin@gmail.com>
2017-06-06 22:57:47 -0700

    Add spring libraries

---
+++ pam-service-core/src/main/java/com/redflamelabs/pam/service/PamServiceApp.java
@@ -0,0 +1,17 @@
+package com.redflamelabs.pam.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for running the application
+ *
+ * Created by kavinarasu on 6/6/17.
+ */
+@SpringBootApplication
+public class PamServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PamServiceApp.class, args);
+    }
+}
